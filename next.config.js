3d32422c563374c246f338ee0a70f92a2e2dc587
Thats-Blazic <urosbl6@gmail.com>
2025-03-02 05:34:07 +0100

    Fix build error by installing critters and simplifying Next.js config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['i.ibb.co'], // Dodajemo i.ibb.co kao dozvoljeni domen
     remotePatterns: [
       {
         protocol: 'https',
@@ -9,33 +8,10 @@ const nextConfig = {
         pathname: '/**',
       },
     ],
-    // Optimizacija slika
-    formats: ['image/avif', 'image/webp'],
-    minimumCacheTTL: 60,
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256],
   },
   eslint: {
-    ignoreDuringBuilds: true, // Privremeno ignorišemo ESLint tokom builda
+    ignoreDuringBuilds: true,
   },
-  // Uključujemo optimizaciju fontova
-  optimizeFonts: true,
-  // Poboljšanje performansi
-  swcMinify: true,
-  compiler: {
-    removeConsole: process.env.NODE_ENV === 'production',
-  },
-  experimental: {
-    // Optimizacija učitavanja
-    optimizeCss: true,
-    // Poboljšanje performansi renderovanja
-    scrollRestoration: true,
-  },
-  // Optimizacija za produkciju
-  reactStrictMode: true,
-  poweredByHeader: false,
-  compress: true,
-  domains: ['fonts.googleapis.com', 'fonts.gstatic.com'],
 }
 
 module.exports = nextConfig 
